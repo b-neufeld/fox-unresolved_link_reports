9e34a3a3735b5f0d3f34b26d3db69628714bc777
Brahm Neufeld <neufeld@gmail.com>
2017-06-04 14:22:48 -0600

    Correction in instructions

--- unresolved_link_report.sh
+++ unresolved_link_report.sh
@@ -4,7 +4,7 @@
 # - Open command prompt
 # - start Kornshell with "ncenv" command (no output) 
 # - Enter shell with "sh" command
-# - type "vpo_unresolved_link_report.sh"
+# - type "unresolved_link_report.sh"
 
 # make MAPOFFSETS.txt file. 
 /opt/fox/bin/tools/mkblkma.sh /usr/fox/sp/files/FCP270.MAP 
@@ -13,10 +13,9 @@
 cp /opt/fox/bin/tools/BlockTypeMap.txt .
 
 # Run /opt/fox/bin/tools/dbvu270 for all CPs
-# Replace LTRBG1,2,3 with your FCP270 letterbugs. Add as many lines as you need!
+# Replace LTRBG1/2/3 with your FCP270 letterbugs. Add as many lines as you need!
 
 /opt/fox/bin/tools/dbvu270 -uredlb -I /usr/fox/sp/files/OS1C70 -O MapOffsets.txt -D /usr/fox/sp/files/DBLTRBG1.UC -f > /opt/customer/unresolved_link_reports/LTRBG1_unresolved_link_report.txt
 /opt/fox/bin/tools/dbvu270 -uredlb -I /usr/fox/sp/files/OS1C70 -O MapOffsets.txt -D /usr/fox/sp/files/DBLTRBG2.UC -f > /opt/customer/unresolved_link_reports/LTRBG2_unresolved_link_report.txt
 /opt/fox/bin/tools/dbvu270 -uredlb -I /usr/fox/sp/files/OS1C70 -O MapOffsets.txt -D /usr/fox/sp/files/DBLTRBG3.UC -f > /opt/customer/unresolved_link_reports/LTRBG3_unresolved_link_report.txt
-
 
